e55eaaab32fac5a6132e54a71d97808865f96f69
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2014-01-14 21:17:34 +0100

    Start from scratch
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- Android.mk
+++ Android.mk
@@ -3,10 +3,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PACKAGE_NAME := template
+LOCAL_PACKAGE_NAME := KitKatWhite
 
 LOCAL_AAPT_FLAGS := -c mdpi,hdpi,xhdpi,xxhdpi -x 10
-
-LOCAL_CERTIFICATE := releasekey
 
 include $(BUILD_PACKAGE)
